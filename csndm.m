242b46682cd80f828f03a660b14cc95534c8c270
Hao Dai <36808242+wys8c764@users.noreply.github.com>
2018-12-15 11:05:37 +0800

    Update csndm.m

--- csndm.m
+++ csndm.m
@@ -2,13 +2,12 @@ function ndm = csndm(data,alpha,boxsize,normalize)
 %Construction of network degree matrix
 %The function performs the transformation from gene expression matrix to
 %network degree matrix (ndm).
-%data: Gene expression matrix, rows = genes, columns = cells
+%data: Gene expression matrix (TPM/RPKM/FPKM/count), rows = genes, columns = cells
 %alpha: Significant level (eg. 0.001, 0.01, 0.05 ...), Default = 0.01
-%boxsize: Size of neighborhood, Default = 0.1
-%normalize: 1  result is normalized (Default)
-%           0  result is not normalized
-
-if nargin < 4
+%boxsize: Size of neighborhood, Default = 0.1 (nx(k) = ny(k) = 0.1*n)
+%normalize: 1  result is normalized (Default); 0  result is not normalized           
+ 
+if nargin < 4 || isempty(normalize)
     normalize = 1;
 end
 if nargin < 3 || isempty(boxsize)
@@ -17,7 +16,7 @@ end
 if nargin <2 || isempty(alpha)
     alpha = 0.01;
 end
-
+ 
 %Define the neighborhood of each plot
 [n1,n2] = size(data);
 upper = zeros(n1,n2);
@@ -42,31 +41,28 @@ for i = 1 : n1
         k = k+s+1;
     end
 end
-
+ 
 %If gene expression matrix is sparse, use the sparse matrix will accelerate
-%the calculation and reduce memory footprint 
+%the calculation and reduce memory footprint
 %data = sparse(data); upper = sparse(upper); lower = sparse(lower);
-
+ 
 %Construction of network degree matrix
 ndm = zeros(n1,n2);
 B = zeros(n1,n2);
+p = -icdf('norm',alpha,0,1);
 for k = 1 : n2
     for j = 1 : n2
-        B(:,j) = (data(:,j) <= upper(:,k) & data(:,j) >= lower(:,k));
+        B(:,j) = data(:,j) <= upper(:,k) & data(:,j) >= lower(:,k) & data(:,k);
     end
+    %B = sparse(B);
     a = sum(B,2);
-    e = sign(data(:,k));
-    d = (B*B'*n2-a*a')./sqrt((a*a').*((n2-a)*(n2-a)')/(n2-1));
-    d = (d.*(1-eye(n1)).*(e*e') > -icdf('norm',alpha,0,1));
-    ndm(:,k) = sum(d,2);
+    csn = (B*B'*n2-a*a')./sqrt((a*a').*((n2-a)*(n2-a)')/(n2-1)+eps);
+    csn = (csn > p);
+    ndm(:,k) = sum(csn,2) - diag(csn);
     disp(['Cell ' num2str(k) ' is completed']);
 end
-
+ 
 %Normalization of network degree matrix
 if normalize
-    a = sum(ndm);
-    a0 = mean(sum(sign(data)))^2/2000;
-    for i = 1 : n1
-        ndm(i,:) = ndm(i,:)./a*a0;
-    end
+    ndm = bsxfun(@rdivide,ndm,sum(ndm))*mean(sum(sign(ndm)))^2/2000;
 end
